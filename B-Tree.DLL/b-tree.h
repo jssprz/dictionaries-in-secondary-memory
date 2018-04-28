28b5b43033c51c8a4be43951e10617deefd020ee
Jesus Perez Martin <jperezmartin90@gmail.com>
2018-04-27 21:44:07 -0300

    Implements btree's search

--- B-Tree.DLL/b-tree.h
+++ B-Tree.DLL/b-tree.h
@@ -1,5 +1,7 @@
 #pragma once
+
 #include "b-tree-node.h"
+#include "error-code.h"
 #include <fstream>
 
 template <typename K, typename R>
@@ -14,13 +16,33 @@ public:
 	}
 
 	//main functions
-	bool search(R &target){}
+	Error_code search(R &target){
+		return recursive_search_tree(root, target);
+	}
 	void insert(const R &new_entry){}
 	void remove(const R &target){}
 
 protected:
 private:
-	bool recursive_search(Node *current, R &target){}
+	Error_code recursive_search(Node *current, R &target){
+		Error_code result = not_present;
+		size_t position;
+		if (current != NULL) {
+			result = search_node(current, target, position);
+			if (result == not_present)
+				result = recursive_search_tree(current->branch[position], target);
+			else
+				target = current->data[position];
+		}
+		return result;
+	}
+
+	Error_code search_node(Node *current, R &target, size_t &position) {
+		position = 0;
+		while (position < current->count && target > current->data[position])
+			position++; // Perform a sequential search through the keys.
+		position < current->count && target == current->data[position] ? return success : not_present;
+	}
 	
 	void push_down(Node *current, const R &new_entry, R &median, Node * &right_branch){}
 	

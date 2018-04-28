de6a129b895b69a85adb509cbbfc280983589fa4
Jesus Perez Martin <jperezmartin90@gmail.com>
2018-04-27 22:01:31 -0300

    Removes b-tree-node.cpp file and move the implementation for the template class to the header file

--- B-Tree.DLL/b-tree-node.h
+++ B-Tree.DLL/b-tree-node.h
@@ -1,13 +1,26 @@
 ﻿#pragma once
 
 template <typename K, typename R>
-struct BTreeNode {
+class BTreeNode {
+
+public:
+
+	BTreeNode(int branchingFactor) {
+		this->data = new Key[branchingFactor - 1];
+		this->branch = new BTreeNode<Key, Record>*[branchingFactor];
+	}
+
+	~BTreeNode() {
+		delete[] this->data;
+		for (size_t i = 0; i < count; i++) {
+			delete this->branch[i];
+		}
+		delete[] this->branch;
+	}
+
 	// data members:
 	int count;
 	K *data;
 	BTreeNode<K, R> **branch;
-	// constructor:
-	BTreeNode(int branchingFactor);
-	// destructor
-	~BTreeNode();
+	
 };

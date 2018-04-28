67820de8151e01bc315628fa34f85842fcbdca1b
Jesus Perez Martin <jperezmartin90@gmail.com>
2018-04-27 23:51:46 -0300

    Simple version of experiments on btree

--- Experiments.Console/main.cpp
+++ Experiments.Console/main.cpp
@@ -1,8 +1,42 @@
-#include "iostream"
+
+#include <iostream>
+#include "b-tree.h"
+
 
 using namespace std;
+using namespace btree;
+
+
+char gen_rand() {
+	static const char alpha[] = "ATCG";
+	return alpha[rand() % (sizeof(alpha) - 1)];
+}
+
+string generate_adn(const size_t length){
+	// If you use C++'s std::string:
+	string str;
+	for (size_t i = 0; i < length; ++i) {
+		str += gen_rand();
+	}
+	return str;
+}
 
 int main() {
 	cout << "Hy" << endl;
+
+	auto db = new BTree<string, string>(20);
+
+	cout << "generating..." << endl;
+	string s = generate_adn(15);
+
+	cout << "inserting..." << endl;
+	cout << db->insert(s) << endl;
+
+	cout << "searching..." << endl;
+	cout << int(db->search(s)) << endl;
+
+	cout << "searching..." << endl;
+	cout << int(db->remove(s)) << endl;
+
 	return 0;
 }
